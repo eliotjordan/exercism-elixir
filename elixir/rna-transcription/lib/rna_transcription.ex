4273fa4ce586f439a66918b63ecbf07147f4ead7
Anna Headley <anna.headley@gmail.com>
2020-02-26 11:56:55 -0500

    Make the first rna test pass

--- elixir/rna-transcription/lib/rna_transcription.ex
+++ elixir/rna-transcription/lib/rna_transcription.ex
@@ -1,5 +1,20 @@
 defmodule RnaTranscription do
   @doc """
+  Transcribes a single character representing a DNA nucleotide to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('G')
+  'C'
+  """
+  @spec to_rna([char]) :: [char]
+  def to_rna(char) when length(char) == 1 do
+    if char == 'G' do
+      'C'
+    end
+  end
+
+  @doc """
   Transcribes a character list representing DNA nucleotides to RNA
 
   ## Examples
