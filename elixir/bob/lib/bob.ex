364703d04401c7e7a783ec53b8f5cee981b5d56f
Anna Headley <anna.headley@gmail.com>
2020-05-13 10:55:51 -0400

    Finish Bob challenge

--- elixir/bob/lib/bob.ex
+++ elixir/bob/lib/bob.ex
@@ -1,14 +1,29 @@
 defmodule Bob do
   def hey(input) do
+    input = Regex.replace(~r/[[:space:]]/, input, "")
     cond do
+      silence?(input) -> "Fine. Be that way!"
+      forceful_question?(input) -> "Calm down, I know what I'm doing!"
       yelling?(input) -> "Whoa, chill out!"
-      yelling?(input) -> "Whoa, chill out!"
+      question?(input) -> "Sure."
       true -> "Whatever."
     end
+  end
+
+  def silence?(input) do
+    input == ""
+  end
+
+  def forceful_question?(input) do
+    yelling?(input) && question?(input)
   end
 
   def yelling?(input) do
     Regex.replace(~r/[^[:alpha:]]/, input, "")
     |> String.match?(~r/^[[:upper:]]+$/)
+  end
+
+  def question?(input) do
+    String.match?(input, ~r/\?$/)
   end
 end
